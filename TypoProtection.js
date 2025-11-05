ebcf5da4922454f1b947bcba5ba50b7ec1a715e4
darknessmain2 <125643358+mosttr@users.noreply.github.com>
2025-11-05 20:39:31 +0300

    Create TypoProtection.js

---
+++ TypoProtection.js
@@ -0,0 +1,51 @@
+[
+
+      {
+    "id": 1,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "robloox.com", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 2,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "rooblox.com", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 3,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "roblox1.com", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 4,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "roblox2.com", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 5,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "robloxx.com", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 6,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "youtubee.com", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 7,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "steamm.com/lander", "resourceTypes": ["main_frame"] }
+  },
+  {
+    "id": 8,
+    "priority": 1,
+    "action": { "type": "redirect", "redirect": { "extensionPath": "/typoProtect.html" } },
+    "condition": { "urlFilter": "crackshash.com/malwarebytes", "resourceTypes": ["main_frame"] }
+  }
+]
